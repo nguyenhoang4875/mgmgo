57eda2dfc1fbe356bed6dbaf18b9f84397b94814
DUONG HUY VU <hv0310197@gmail.com>
2019-06-17 13:58:33 +0700

    EXP-43 Create table database

---
+++ src/main/resources/dbschema/testdata/category_test.sql
@@ -0,0 +1,11 @@
+INSERT INTO category (name)
+VALUES ('Event');
+
+INSERT INTO category (name)
+VALUES ('Restaurant');
+
+INSERT INTO category (name)
+VALUES ('Coffee shop');
+
+INSERT INTO category (name)
+VALUES ('Swim');
