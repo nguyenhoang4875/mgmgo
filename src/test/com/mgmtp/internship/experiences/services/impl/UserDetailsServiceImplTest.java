a81971f058369e1de298c4247febff0d689fc5c5
Khanh <vkhanh234@gmail.com>
2019-06-17 17:01:33 +0700

    EXP-42 Unit test for spring security login

---
+++ src/test/com/mgmtp/internship/experiences/services/impl/UserDetailsServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.mgmtp.internship.experiences.services.impl;
+
+import com.mgmtp.internship.experiences.config.security.CustomUserDetails;
+import com.mgmtp.internship.experiences.model.tables.tables.records.UserRecord;
+import com.mgmtp.internship.experiences.repositories.UserRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserDetailsServiceImplTest {
+    private static final long EXPECTEDID = 1L;
+    private static final String EXPECTEDUSERNAME = "username";
+    private static final String EXPECTEDPASSWORD = "password";
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Test
+    public void shouldReturnUserDetails() {
+        UserRecord expectedUserRecord = new UserRecord(EXPECTEDID, EXPECTEDUSERNAME, EXPECTEDPASSWORD);
+        UserDetails expectedUserDetails = new CustomUserDetails(expectedUserRecord.getId(), expectedUserRecord.getUsername(), expectedUserRecord.getPassword(), Collections.emptyList());
+        Mockito.when(userRepository.findUserByUsername(EXPECTEDUSERNAME)).thenReturn(expectedUserRecord);
+
+        UserDetails actualUserDetails = userDetailsService.loadUserByUsername(EXPECTEDUSERNAME);
+
+        Assert.assertEquals(expectedUserDetails, actualUserDetails);
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void shouldReturnUsernameNotFoundException() {
+        Mockito.when(userRepository.findUserByUsername(EXPECTEDUSERNAME)).thenReturn(null);
+
+        UserDetails actualUserDetails = userDetailsService.loadUserByUsername(EXPECTEDUSERNAME);
+    }
+}
